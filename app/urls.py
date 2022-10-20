8e80aa4a4237ae17d5a2948b97ec50e88809d5c9
al-jaber-nishad <mailnishad02@gmail.com>
2022-10-20 15:18:40 +0600

    Updated urls for login slug

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path("setting/",views.setting, name='setting'),
 
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 
