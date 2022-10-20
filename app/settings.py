b8dd4bc54fbf1e2ceb040ddecd87592a0569c7d3
al-jaber-nishad <mailnishad02@gmail.com>
2022-10-20 15:23:12 +0600

    Updated

--- app/settings.py
+++ app/settings.py
@@ -149,3 +149,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
+# LOGIN_REDIRECT_URL = '/registration/profile/'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
